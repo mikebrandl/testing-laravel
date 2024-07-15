01f292cd72d2803362b3c74cd1dc574111f2efaf
Mike Brandl <mike.brandl@cityfibre.com>
2024-07-15 10:55:36 +0100

    Show Data Provider Usage in Unit Tests

---
+++ tests/Feature/DataProviderToTestValidationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Fruit;
+use Tests\FeatureTestCase;
+
+class DataProviderToTestValidationTest extends FeatureTestCase
+{
+    /**
+     * @dataProvider provideCreationData
+     */
+    public function testCreateFruit($data, $code): void
+    {
+        $this->postJson('/fruit', $data)->assertStatus($code);
+    }
+
+    public static function provideCreationData()
+    {
+        return [
+            [
+                // Fruit::factory()->make()->toArray(), -- This doesn't work in a unit test, so we have to manually specify the data.
+                ['name' => 'apple', 'color' => 'red'],
+                201,
+            ],
+            [
+                ['name' => 'banana'],
+                422,
+            ],
+        ];
+    }
+}
