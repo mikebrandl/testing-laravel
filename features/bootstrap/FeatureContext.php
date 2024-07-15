b343d5c6bec820b68ac7b92ecb62510f09bf507f
Mike Brandl <mike.brandl@cityfibre.com>
2024-07-15 15:59:43 +0100

    Run Behat

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,57 +1,5 @@
 <?php
 
-use App\Objects\Basket;
-use App\Objects\Shelf;
 use Behat\Behat\Context\SnippetAcceptingContext;
-use PHPUnit\Framework\Assert;
 
-class FeatureContext implements SnippetAcceptingContext
-{
-    private $shelf;
-
-    private $basket;
-
-    public function __construct()
-    {
-        $this->shelf = new Shelf();
-        $this->basket = new Basket($this->shelf);
-    }
-
-    /**
-     * @Given there is a :product, which costs £:price
-     */
-    public function thereIsAWhichCostsPs($product, $price)
-    {
-        $this->shelf->setProductPrice($product, floatval($price));
-    }
-
-    /**
-     * @When I add the :product to the basket
-     */
-    public function iAddTheToTheBasket($product)
-    {
-        $this->basket->addProduct($product);
-    }
-
-    /**
-     * @Then I should have :count product(s) in the basket
-     */
-    public function iShouldHaveProductInTheBasket($count)
-    {
-        Assert::assertCount(
-            intval($count),
-            $this->basket
-        );
-    }
-
-    /**
-     * @Then the overall basket price should be £:price
-     */
-    public function theOverallBasketPriceShouldBePs($price)
-    {
-        Assert::assertSame(
-            floatval($price),
-            $this->basket->getTotalPrice()
-        );
-    }
-}
+class FeatureContext implements SnippetAcceptingContext {}
